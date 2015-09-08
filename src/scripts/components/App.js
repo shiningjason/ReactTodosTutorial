0cf19fcbf48a63c3d0808028c32d910c1f0ec817
Chung Jason <shiningjason1989@gmail.com>
2015-09-08 22:00:29 +0800

    Add default todos data

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import Header from './Header';
 
+const DEFAULT_TODOS = [
+  {
+    id: 1,
+    content: '準備 React & Flux 教育訓練',
+    completed: false
+  },
+  {
+    id: 2,
+    content: '繳電話費',
+    completed: false
+  },
+  {
+    id: 3,
+    content: '繳房租',
+    completed: false
+  },
+  {
+    id: 4,
+    content: '週會會議記錄',
+    completed: true
+  }
+];
+
 export default class App extends React.Component {
   render() {
     return (
