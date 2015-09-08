2fd7150e36738dcbed6ad0997c92d80a8cd02de0
Chung Jason <shiningjason1989@gmail.com>
2015-09-08 17:34:23 +0800

    Remove .js suffix at required module name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 require('../assets/stylesheets/style.sass');
 
 import React from 'react';
-import App from './components/App.js';
+import App from './components/App';
 
 React.render(<App />, document.getElementById('main'));
