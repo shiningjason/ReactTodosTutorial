7a9d816f5fdd84e82670ef46eba775488e2668b2
Chung Jason <shiningjason1989@gmail.com>
2015-09-08 17:35:10 +0800

    Add Header component

---
+++ src/scripts/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Todos</h1>
+        <p>哈囉，Jason：您今天有 100 條代辦事項！</p>
+      </header>
+    );
+  }
+}
