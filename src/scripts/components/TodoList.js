566e2b94c3283def7c1caa06dbc7b4ff9ffdc8ac
Chung Jason <shiningjason1989@gmail.com>
2015-09-08 22:21:39 +0800

    Add TodoList component

---
+++ src/scripts/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends React.Component {
+  render() {
+    const { todos } = this.props;
+    const todoItems = todos.map((todo) => (
+      <li>
+        <TodoItem content={todo.content} completed={todo.completed} />
+      </li>
+    ));
+
+    return (
+      <ul>
+        {todoItems}
+      </ul>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  todos: React.PropTypes.array.isRequired
+};
