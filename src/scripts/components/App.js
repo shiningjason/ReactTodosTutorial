039fed56b2399bf203da77d0f93e94b7bb4e3d7d
Chung Jason <shiningjason1989@gmail.com>
2015-09-08 17:36:07 +0800

    Update App to display header component

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 export default class App extends React.Component {
   render() {
-    var codeBlockStyle = { "fontFamily": "monospace",
-                           "backgroundColor": "#D0D0D0" };
     return (
-    <div>
-      <header>
-        <h1>ReactTodosTutorial</h1>
-        <h3>A React/Flux app generate by RF, powered with Babel</h3>
-      </header>
-      <article className="context">
-        <p>
-          Greeting form <a href="https://github.com/taiansu/generator-rf">RF generator</a>.
-        </p>
-
-        <p>
-          Remember you are powered with <a href="https://gaearon.github.io/react-hot-loader/">react-hot-loader</a> now. Edit this file in <span style={ codeBlockStyle }>src/scripts/components/App.js</span> and save it, it will auto reload the page for you.
-        </p>
-      </article>
-    </div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
