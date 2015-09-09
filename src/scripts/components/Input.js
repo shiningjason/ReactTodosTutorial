3ef4a5393dbcd3509a73a85b7e529cd0b1509732
Chung Jason <shiningjason1989@gmail.com>
2015-09-09 13:46:38 +0800

    Update Input to handle change

--- src/scripts/components/Input.js
+++ src/scripts/components/Input.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default class Input extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
-      <input type="text" style={styles.textField} />
+      <input
+        type="text"
+        style={styles.textField}
+        value={this.state.value}
+        onChange={this.handleChange.bind(this)} />
     );
   }
 }
