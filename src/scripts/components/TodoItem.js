f56ef69185ff2bd6d9594a6dbf82f3f351047d80
Chung Jason <shiningjason1989@gmail.com>
2015-09-09 11:53:49 +0800

    Update TodoItem styles

--- src/scripts/components/TodoItem.js
+++ src/scripts/components/TodoItem.js
@@ -4,9 +4,9 @@ export default class TodoItem extends React.Component {
   render() {
     const { content, completed } = this.props;
     return (
-      <div>
-        <input type="checkbox" checked={completed} />
-        <label>{content}</label>
+      <div style={styles.container}>
+        <input type="checkbox" style={styles.checkbox} checked={completed} />
+        <label style={styles.content}>{content}</label>
       </div>
     );
   }
@@ -15,4 +15,19 @@ export default class TodoItem extends React.Component {
 TodoItem.propTypes = {
   content: React.PropTypes.string.isRequired,
   completed: React.PropTypes.bool.isRequired
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 21
+  },
+  checkbox: {
+    marginRight: 16,
+    marginLeft: 16
+  },
+  content: {
+    color: 'rgba(0,0,0,0.87)'
+  }
 };
