75ee752c151ec203ad21d4c3c1ee45313bd67c71
Chung Jason <shiningjason1989@gmail.com>
2015-09-09 11:53:29 +0800

    Update TodoList styles

--- src/scripts/components/TodoList.js
+++ src/scripts/components/TodoList.js
@@ -5,13 +5,13 @@ export default class TodoList extends React.Component {
   render() {
     const { todos } = this.props;
     const todoItems = todos.map((todo) => (
-      <li key={todo.id}>
+      <li key={todo.id} style={styles.todoItem}>
         <TodoItem content={todo.content} completed={todo.completed} />
       </li>
     ));
 
     return (
-      <ul>
+      <ul style={styles.todoList}>
         {todoItems}
       </ul>
     );
@@ -20,4 +20,17 @@ export default class TodoList extends React.Component {
 
 TodoList.propTypes = {
   todos: React.PropTypes.array.isRequired
+};
+
+const styles = {
+  todoList: {
+    padding: 0,
+    listStyle: 'none',
+    borderTop: '1px solid rgba(0,0,0,0.12)'
+  },
+  todoItem: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    borderBottom: '1px solid rgba(0,0,0,0.12)'
+  }
 };
