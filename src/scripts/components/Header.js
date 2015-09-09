f36e5cac7953dcacbeecccb9afe1a5e283b95f90
Chung Jason <shiningjason1989@gmail.com>
2015-09-09 14:54:43 +0800

    Fixed typo

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
     return (
       <header>
         <h1>Todos</h1>
-        <p>哈囉，{username}：您今天有 {todoNumber} 條代辦事項！</p>
+        <p>哈囉，{username}：您今天有 {todoNumber} 條待辦事項！</p>
       </header>
     );
   }
