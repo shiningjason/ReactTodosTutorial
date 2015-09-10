add8fd00a10a1b8c67c12ee4346f17342d5b9bda
Chung Jason <shiningjason1989@gmail.com>
2015-09-10 11:14:55 +0800

    Update TodoStore to handle action

--- src/scripts/stores/TodoStore.js
+++ src/scripts/stores/TodoStore.js
@@ -1,6 +1,58 @@
 import { EventEmitter } from 'events';
+import shortid from 'shortid';
+import ActionTypes from '../constants/ActionTypes';
+import AppDispatcher from '../dispatcher/AppDispatcher';
 
 const CHANGE_EVENT = 'change';
+const DEFAULT_TODOS = [
+  {
+    id: shortid(),
+    content: '準備 React & Flux 教育訓練',
+    completed: false
+  },
+  {
+    id: shortid(),
+    content: '繳電話費',
+    completed: false
+  },
+  {
+    id: shortid(),
+    content: '繳房租',
+    completed: false
+  },
+  {
+    id: shortid(),
+    content: '週會會議記錄',
+    completed: true
+  }
+];
+
+const _addTodo = (todos, content) => {
+  todos.push({
+    id: shortid(),
+    content,
+    completed: false
+  });
+  return todos;
+};
+
+const _toggleTodo = (todos, id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  return todos;
+};
+
+const _deleteTodo = (todos, id) => {
+  const idx = todos.findIndex((todo) => todo.id === id);
+  todos.splice(idx, 1);
+  return todos;
+};
+
+const _editTodo = (todos, id, content) => {
+  const todo = todos.find((todo) => todo.id === id);
+  todo.content = content;
+  return todos;
+};
 
 const TodoStore = {
   ...EventEmitter.prototype,
@@ -21,5 +73,31 @@ const TodoStore = {
     this.removeListener(CHANGE_EVENT, callback);
   }
 };
+
+var _todos = DEFAULT_TODOS;
+
+AppDispatcher.register((action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_TODO:
+      _todos = _addTodo(_todos, action.content);
+      TodoStore.emitChange();
+      break;
+
+    case ActionTypes.EDIT_TODO:
+      _todos = _editTodo(_todos, action.id, action.content);
+      TodoStore.emitChange();
+      break;
+
+    case ActionTypes.TOGGLE_TODO:
+      _todos = _toggleTodo(_todos, action.id);
+      TodoStore.emitChange();
+      break;
+
+    case ActionTypes.DELETE_TODO:
+      _todos = _deleteTodo(_todos, action.id);
+      TodoStore.emitChange();
+      break;
+  }
+});
 
 export default TodoStore;
