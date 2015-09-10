c65715db9248678a43b5f6a8c904a23aa19914c4
Chung Jason <shiningjason1989@gmail.com>
2015-09-10 16:49:43 +0800

    Update action to return action payload

--- src/scripts/actions/TodoActions.js
+++ src/scripts/actions/TodoActions.js
@@ -1,31 +1,22 @@
 import ActionTypes from '../constants/ActionTypes';
-import AppDispatcher from '../dispatcher/AppDispatcher';
 
-export const addTodo = (content) => {
-  AppDispatcher.dispatch({
-    type: ActionTypes.ADD_TODO,
-    content
-  });
-};
+export const addTodo = (content) => ({
+  type: ActionTypes.ADD_TODO,
+  content
+});
 
-export const editTodo = (id, content) => {
-  AppDispatcher.dispatch({
-    type: ActionTypes.EDIT_TODO,
-    id,
-    content
-  });
-};
+export const editTodo = (id, content) => ({
+  type: ActionTypes.EDIT_TODO,
+  id,
+  content
+});
 
-export const toggleTodo = (id) => {
-  AppDispatcher.dispatch({
-    type: ActionTypes.TOGGLE_TODO,
-    id
-  });
-};
+export const toggleTodo = (id) => ({
+  type: ActionTypes.TOGGLE_TODO,
+  id
+});
 
-export const deleteTodo = (id) => {
-  AppDispatcher.dispatch({
-    type: ActionTypes.DELETE_TODO,
-    id
-  });
-};
+export const deleteTodo = (id) => ({
+  type: ActionTypes.DELETE_TODO,
+  id
+});
